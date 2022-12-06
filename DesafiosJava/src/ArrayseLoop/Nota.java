2ab4b6b1bb11b55c7e7a877db76aed578bafc27c
RebecaPonciano <oi@mentorama.com>
2022-12-06 20:46:32 -0300

    Adicionando exercicio nota

---
+++ DesafiosJava/src/ArrayseLoop/Nota.java
@@ -0,0 +1,22 @@
+package ArrayseLoop;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+public class Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota:  ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println(" Atenção! Nota inválida! Digite novamente:   ");
+            nota = scan.nextInt();
+
+        }
+    }
+
+}
